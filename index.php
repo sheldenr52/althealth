f43403cebbc646dcd9ca774557fb650bee6c9576
sheldenr52 <66136979+sheldenr52@users.noreply.github.com>
2020-07-11 01:39:34 +0200

    added input GUIs named index.php and new client.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Input Gui- Display Existing Client and Capture new client</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div>
+        <nav class="navbar navbar-light navbar-expand-md navigation-clean">
+            <div class="container"><a class="navbar-brand" href="#" style="color:rgb(57,149,0);">AltHealth</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div
+                    class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav mx-auto"></ul>
+            </div>
+    </div>
+    </nav>
+    </div>
+    <div class="jumbotron" style="text-align:center;"><button class="btn btn-primary" type="button" style="margin:20px;">Display Existing Client</button><button class="btn btn-primary" type="button">Capture New Client</button>
+        <div style="height:0px;background-color:#b8efbd;"></div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
